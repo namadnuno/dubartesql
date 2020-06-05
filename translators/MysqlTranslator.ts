8b04db170f3bd02538558baf6f2e95a8f9a23fa9
Nuno Alexandre <n.alexandre@xgeeks.io>
2020-06-05 12:59:04 +0100

    feat(count-method): add count method
    now is possible to do something like Model.count() ....

--- translators/MysqlTranslator.ts
+++ translators/MysqlTranslator.ts
@@ -1,89 +1,105 @@
-import {QueryBuilder} from "../QueryBuilder.ts";
+import { QueryBuilder } from "../QueryBuilder.ts";
 
 export class MysqlTranslator<T> {
-    private queryBuilder: QueryBuilder<T>;
-    constructor(queryBuilder: QueryBuilder<T>) {
-        this.queryBuilder = queryBuilder;
+  private queryBuilder: QueryBuilder<T>;
+  constructor(queryBuilder: QueryBuilder<T>) {
+    this.queryBuilder = queryBuilder;
+  }
+
+  getSelectsSql() {
+    if (this.queryBuilder.isCount()) {
+      return "count(*) as count";
     }
 
-     getSelectsSql() {
-        if (this.queryBuilder.selects.length == 0) {
-            return '*';
-        }
-
-        return this.queryBuilder.selects.join(',');
+    if (this.queryBuilder.selects.length == 0) {
+      return "*";
     }
 
-    sqlBegin() {
-        let begin = '';
-        switch (this.queryBuilder.type) {
-            case 'insert':
-                begin = 'insert';
-                break;
-            case 'delete':
-                begin = 'delete';
-                break;
-            case 'update':
-                begin = 'update';
-                break;
-            default:
-                begin = `select ${this.getSelectsSql()}`;
-        }
+    return this.queryBuilder.selects.join(",");
+  }
 
-        return begin;
+  sqlBegin() {
+    let begin;
+    switch (this.queryBuilder.type) {
+      case "insert":
+        begin = "insert";
+        break;
+      case "delete":
+        begin = "delete";
+        break;
+      case "update":
+        begin = "update";
+        break;
+      default:
+        begin = `select ${this.getSelectsSql()}`;
     }
 
-    sqlFrom() {
-        if (this.queryBuilder.type == 'insert') {
-            return `into ${this.queryBuilder.table}`;
-        }
+    return begin;
+  }
 
-        if (this.queryBuilder.type == 'update') {
-            return ` ${this.queryBuilder.table}`;
-        }
-
-        return `from ${this.queryBuilder.table}`;
+  sqlFrom() {
+    if (this.queryBuilder.type == "insert") {
+      return `into ${this.queryBuilder.table}`;
     }
 
-    sqlMidle() {
-        if (this.queryBuilder.type == 'insert') {
-            return `(${Object.keys(this.queryBuilder.fieldsToUpdateOrInsert).join(',')}) values ('${Object.values(this.queryBuilder.fieldsToUpdateOrInsert).join("','")}')`;
-        }
-
-        if (this.queryBuilder.wheres.length > 0) {
-            return "where " + this.queryBuilder.wheres.reduce((acc: any, where:any) => {
-                return [ ...acc, `${where.field}${where.operator}"${where.value}" `]
-            }, []).join(" AND ");
-        }
-
-        return "";
+    if (this.queryBuilder.type == "update") {
+      return ` ${this.queryBuilder.table}`;
     }
 
-    sqlSet(){
-        if (this.queryBuilder.type == 'update') {
-            return "SET " + Object.keys(this.queryBuilder.fieldsToUpdateOrInsert).reduce((acc: any, key) => {
-                return [ ...acc, `${key}="${this.queryBuilder.fieldsToUpdateOrInsert[key]}"`]
-            }, []).join(",");
-        }
+    return `from ${this.queryBuilder.table}`;
+  }
 
-        return "";
+  sqlMidle() {
+    if (this.queryBuilder.type == "insert") {
+      return `(${
+        Object.keys(this.queryBuilder.fieldsToUpdateOrInsert).join(",")
+      }) values ('${
+        Object.values(this.queryBuilder.fieldsToUpdateOrInsert).join("','")
+      }')`;
     }
 
-    sqlTail() {
-        if (this.queryBuilder.isSingle()) {
-            return "limit 1";
-        }
-
-        return "";
+    if (this.queryBuilder.wheres.length > 0) {
+      return "where " +
+        this.queryBuilder.wheres.reduce((acc: any, where: any) => {
+          return [...acc, `${where.field}${where.operator}"${where.value}" `];
+        }, []).join(" AND ");
     }
 
-    translate() {
-        let begin = this.sqlBegin();
-        let from = this.sqlFrom();
-        let set = this.sqlSet();
-        let middle = this.sqlMidle();
-        let tail = this.sqlTail();
+    return "";
+  }
 
-        return `${begin} ${from} ${set} ${middle} ${tail}`;
+  sqlSet() {
+    if (this.queryBuilder.type == "update") {
+      return "SET " +
+        Object.keys(this.queryBuilder.fieldsToUpdateOrInsert).reduce(
+          (acc: any, key) => {
+            return [
+              ...acc,
+              `${key}="${this.queryBuilder.fieldsToUpdateOrInsert[key]}"`,
+            ];
+          },
+          [],
+        ).join(",");
     }
+
+    return "";
+  }
+
+  sqlTail() {
+    if (this.queryBuilder.isSingle()) {
+      return "limit 1";
+    }
+
+    return "";
+  }
+
+  translate() {
+    let begin = this.sqlBegin();
+    let from = this.sqlFrom();
+    let set = this.sqlSet();
+    let middle = this.sqlMidle();
+    let tail = this.sqlTail();
+
+    return `${begin} ${from} ${set} ${middle} ${tail}`;
+  }
 }
